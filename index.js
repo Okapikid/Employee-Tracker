c17d748358ad0b519d986afed0ca4f4c731e2fdd
Auggie <miaugus@gmail.com>
2022-08-13 15:56:44 -0400

    tables created according to assignment instructions

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// REQUIRED CONSTANTS
 const express = require("express");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+// ESTABLISH DATABASE CONNECTION
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -12,21 +14,48 @@ const db = mysql.createConnection(
   console.log("Connected to the employee database")
 );
 
+// BEGINNING MENU OPTIONS
 function menu() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "menu",
-      message: "What would you like to do? (Use arrow keys)",
-      choices: [
-        "View All Employees",
-        "Add Employee",
-        "Update Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Department",
-      ],
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do? (Use arrow keys)",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+        ],
+      },
+    ])
+    // SWITCH CASES FOR BEGINNING MENU OPTIONS
+    .then((answers) => {
+      switch (answers.menu) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "Add Employee":
+          viewAddEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "View All Departments":
+          viewAllDepartments();
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
 }
