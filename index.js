7fba86546b2165cde35ae9316798604d5ad5d080
Auggie <miaugus@gmail.com>
2022-08-14 14:40:30 -0400

    view all employees working

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 
 // ESTABLISH DATABASE CONNECTION
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
     password: "",
-    database: "nodemysql",
+    database: "employees_db",
   },
   console.log("Connected to the employee database")
 );
@@ -62,5 +62,14 @@ function menu() {
 
 // FUNCTION FOR VIEW ALL EMPLOYEES
 // function viewAllEmployees() {}
+function viewAllEmployees() {
+  connection.query(
+    `SELECT employee.id AS Employee_ID, CONCAT (employee.first_name, " ", employee.last_name) AS Name, employeeRole.title AS Title, department.name AS Department, employeeRole.salary AS Salary, CONCAT (manager.first_name, " ", manager.last_name) AS Manager FROM employee JOIN employeeRole ON employee.role_id = employeeRole.id JOIN department ON employeeRole.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id ORDER BY employee.id`,
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+    }
+  );
+}
 
 menu();
